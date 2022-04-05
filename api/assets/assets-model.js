829d028d3584333d8e2d37a291f35f417094a8ae
Brett Smith <brettsmith212@gmail.com>
2022-04-04 22:00:14 -0700

    fixed POST, GET by ID and DELETE

--- api/assets/assets-model.js
+++ api/assets/assets-model.js
@@ -1,28 +1,32 @@
 const db = require("../data/db-config");
 
 function getAssets() {
-    return db('assets')
+  return db("assets");
 }
 
-async function addAssets (assets) {
-
-    const [newAssets] = await db("assets").insert(assets)
-    return newAssets;
+async function addAssets(assets) {
+  const [newAssets] = await db("assets").insert(assets, [
+    "asset_name",
+    "asset_price",
+    "user_id",
+  ]);
+  return newAssets;
 }
 
-function getById(id) {
-    return db("assets").where("asset_id", id).first();
-  }
+async function getById(id) {
+  const asset = await db("assets").where("asset_id", id).first();
+  return asset;
+}
 
-function remove(asset_id) {
-    return db('assets').where({ asset_id }).del();
+async function remove(asset_id) {
+  return await db("assets")
+    .where({ asset_id: asset_id })
+    .del(["asset_id", "asset_name", "asset_price"]);
 }
 
 module.exports = {
-
-    getAssets,
-    addAssets,
-    getById,
-    remove,
-
-}
+  getAssets,
+  addAssets,
+  getById,
+  remove,
+};
--- api/assets/assets-router.js
+++ api/assets/assets-router.js
@@ -1,73 +1,57 @@
 const router = require("express").Router();
-const Assets = require('./assets-model');
+const Assets = require("./assets-model");
 
 router.get("/", (req, res, next) => {
+  Assets.getAssets()
+    .then((assets) => {
+      res.json(assets);
+    })
+    .catch(next);
+});
 
-    Assets.getAssets()
-      .then((assets) => {
-        res.json(assets);
-      })
-      .catch(next);
-
-  });
-
-  // receiving error message: "insert into \"assets\" (\"asset_name\", \"asset_value\", \"user_id\") values ($1, $2, $3) returning \"asset_name\", \"asset_value\", \"user_id\" - column \"asset_value\" of relation \"assets\" does not exist",
-
-router.post('/', async (req, res, next) => {
-
-    try {
-        const newPortfolio = await Assets.addAssets({
-          ...req.body,
-        });
-        res.status(201).json(newPortfolio);
-      } catch (err) {
-        next(err);
-      }
-
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const newPortfolio = await Assets.addAssets({
+      ...req.body,
+    });
+    res.status(201).json(newPortfolio);
+  } catch (err) {
+    next(err);
+  }
+});
 
 // PUT Endpoint -- updates specific asset based on asset_id
 
-router.put("/:asset_id", (req, res, next) => {
-  
-})
+router.put("/:asset_id", (req, res, next) => {});
 
 // receiving error message: "Undefined binding(s) detected when compiling FIRST. Undefined column(s): [asset_id] query: select * from \"assets\" where \"asset_id\" = ? limit ?" from posting endpoint
 
 router.get("/:asset_id", async (req, res, next) => {
-
-    try {
-        const asset = await Assets.getById(req.asset_id);
-        if (asset) {
-          res.status(200).json(asset);
-        } else {
-          res.status(401).json({
-            message: `Asset with ID ${req.asset_id} does not exist.`,
-          });
-        }
-      } catch (err) {
-        next(err);
-      }
-
+  try {
+    const asset = await Assets.getById(req.params.asset_id);
+    if (asset) {
+      res.status(200).json(asset);
+    } else {
+      res.status(401).json({
+        message: `Asset with ID ${req.asset_id} does not exist.`,
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
-// receiving error message: "Undefined binding(s) detected when compiling FIRST. Undefined column(s): [asset_id] query: select * from \"assets\" where \"asset_id\" = ? limit ?",
+router.delete("/:asset_id", async (req, res, next) => {
+  try {
+    const assetToDelete = await Assets.remove(req.params.asset_id);
+    if (assetToDelete) {
+      res.status(200).json({ message: `Deleted ${assetToDelete} item.` });
+    } else {
+      res.status(401).json({ message: `Could not locate specific asset.` });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.delete('/asset_id', async (req, res, next) => {
-    
-    try {
-        const assetToDelete = await Assets.remove(req.params.asset_id);
-        if (assetToDelete) {
-        res.status(200).json({message: `Deleted ${assetToDelete} item.`});
-        } else {
-        res.status(401).json({message: `Could not locate specific asset.`})
-        }
-
-      } catch (err) {
-        next(err);
-      }
-    
-})
-
-
-module.exports = router; 
+module.exports = router;
